73a4aa38a52f77cb8c53324b7e5129ff622b8ec0
avi <avithalker@gmail.com>
2019-03-16 14:35:01 +0200

    1. adding the model project 2. adding db connection string

---
+++ Server/Model/TripMeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripMe.Model
+{
+    public class TripMeContext : DbContext
+    {
+        public TripMeContext() : base(nameof(TripMeContext))
+        {
+            Database.SetInitializer<TripMeContext>(null);
+        }
+
+        //public DbSet<Diary> Diaries { get; set; }
+    }
+}
--- Server/TripMeServer/Controllers/DiaryController.cs
+++ Server/TripMeServer/Controllers/DiaryController.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TripMe.Model;
 
 namespace TripMeServer.Controllers
 {
     public class DiaryController : ApiController
     {
+        [HttpGet]
+        [Route("testMe")]
+        public void TestMe()
+        {
+            using (var dbContext = new TripMeContext())
+            {
+               // var result = dbContext.Diaries.FirstOrDefault(d => d.Id == 1);
+            }
+        }
     }
 }
