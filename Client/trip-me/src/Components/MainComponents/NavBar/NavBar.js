f6b4d61aff6eb12808a068c6b2dc8ebcc465d6e8
avi <avithalker@gmail.com>
2019-09-06 17:24:21 +0300

    trip me fucking logo

--- Client/trip-me/src/Components/MainComponents/NavBar/NavBar.js
+++ Client/trip-me/src/Components/MainComponents/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import {
   currentAuthenticatedUser
 } from "../../../Utils/AuthenticationManager.js";
 import "./Navbar.css";
-import TripMeLogo from "../../../sources/images/TripMeLogo.png";
+import TripMeLogo from "../../../sources/images/TripmeLogo.png";
 
 const authenticationManager = new AuthenticationManager();
 
