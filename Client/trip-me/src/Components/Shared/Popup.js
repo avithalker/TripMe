91075fc26a0205577179731d0fc7e5b24c04bd3d
avi <avithalker@gmail.com>
2019-09-07 22:47:51 +0300

    change to logo

--- Client/trip-me/src/Components/Shared/Popup.js
+++ Client/trip-me/src/Components/Shared/Popup.js
@@ -7,7 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
-import TripMeLogo from "../../sources/images/TripMeLogo.png";
+import TripMeLogo from "../../sources/images/logo.png";
 
 class PopUp extends Component {
   constructor(props) {
